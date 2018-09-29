9c39c0a5327f8682ac93a37843f455a01e7e1e43
Aeryan <raunoita@gmail.com>
2018-09-30 02:07:02 +0300

    css fix - god bless SOF

--- Veebirakendus/settings.py
+++ Veebirakendus/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0+_+f6+79$k=@c_4y_#*++2b@he*kfrbw#7hc0p!**k67x@e-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'vl2018-ryhm15.herokuapp.com']
 
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
 import dj_database_url
--- booksearch/views.py
+++ booksearch/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.template import Context
 from django.http import HttpResponse
 import os
 import psycopg2
