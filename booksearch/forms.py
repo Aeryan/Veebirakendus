8b7f44e75fb20df4d00800b68967bc26ed240d42
Aeryan <raunoita@gmail.com>
2018-10-01 16:32:27 +0300

    lisasin koleda tabeli

--- booksearch/forms.py
+++ booksearch/forms.py
@@ -1,22 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
 
 
 class Signup(forms.Form):
     k_nimi = forms.CharField(label='Kasutajanimi', max_length=50,
                              error_messages={'invalid': "Kasutajanimi on juba võetud!"})
     parool = forms.CharField(label='Parool', max_length=30)
-
-    class Meta:
-        model = User
-
-    def clean_username(self):
-        username = self.cleaned_data["k_nimi"]
-
-        try:
-            raise forms.ValidationError(
-                self.error_messages['duplicate_username'],
-                code='duplicate_username'
-            )
-        except User.DoesNotExist:
-            return username
