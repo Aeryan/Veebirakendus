982b186f1db07f82a24565bbcb853b5a13cbef94
Aeryan <raunoita@gmail.com>
2018-10-01 15:43:57 +0300

    hashi suuruse fix

--- booksearch/models.py
+++ booksearch/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class kasutajad(models.Model):
     kasutajanimi = models.CharField(max_length=50, unique=True)
-    parool = models.CharField(max_length=30)
+    parool = models.CharField(max_length=100)
