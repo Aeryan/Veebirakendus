b7c668d02ac9ee41932ef7e1da77654611664a53
JaakobJ <37977470+JaakobJ@users.noreply.github.com>
2018-10-04 19:07:24 +0300

    Lisasin views.frontpage ja views.about

--- booksearch/urls.py
+++ booksearch/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^/$', views.frontpage),
+    url(r'^about/$', views.about),
+    path('', views.index, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)ngs.STATIC_ROOT)
