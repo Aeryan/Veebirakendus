53f6fc87350b94768d893fec035f2a2069790141
Rauno Jaaska <raunoita@gmail.com>
2018-10-24 22:38:49 +0300

    Research (#4)
    * otsingu remake - uus view/url, cache'i kasutus otsingusisendi edasiandmiseks
    
    * search.css täiendused
    
    * cache'i asemel request.sessioni kasutamine

--- booksearch/views.py
+++ booksearch/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
-from .forms import Login, Signup, Search
+from .forms import Login, Signup, Search, AddOwned
 from .models import raamatud
 from django.db import IntegrityError, connection
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
+from django.core.cache import cache
 import bcrypt
 
 salt = b'$2b$12$46cw2.wl5erIKwdMTQqeF.'
@@ -16,11 +17,44 @@ def about(request):
     return render(request, 'booksearch/About.html', {'loginform': loginform, 'signupform': signupform})
 
 
-'''
 def search(request):
-    if request.method == 'POST':
-        search = Search(request.POST)
-'''
+    loginform = Login(None or request.POST)
+    signupform = Signup(None or request.POST)
+
+    if loginform.is_valid():
+        login_name = loginform.cleaned_data['login_k_nimi']
+        login_password = bcrypt.hashpw(loginform.cleaned_data['login_parool'].encode(), salt).decode()
+        user = authenticate(request, username=login_name, password=login_password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'booksearch/Frontpage.html', {'loginform': loginform, 'signupform': signupform,
+                                                                 'search': search})
+    if signupform.is_valid():
+        signup_name = signupform.cleaned_data['signup_k_nimi']
+        signup_password = bcrypt.hashpw(signupform.cleaned_data['signup_parool'].encode(), salt).decode()
+        user = User.objects.create_user(signup_name, password=signup_password)
+        try:
+            user.save()
+        except IntegrityError:
+            return HttpResponseRedirect('')
+        return HttpResponseRedirect('')
+
+    else:
+        sisend = request.session.get('sisend')
+        if sisend is None:
+            return HttpResponseRedirect('/')
+        tulem = raamatud.objects.filter(pealkiri__icontains=sisend)
+        cursor = connection.cursor()
+        cursor.execute("SELECT count(*) FROM booksearch_raamatud WHERE pealkiri ILIKE '%" + sisend + "%'")
+        arv = cursor.fetchone()[0]
+
+        if arv == 1:
+            sone = 'Leiti 1 tulemus'
+        else:
+            sone = 'Leiti ' + str(arv) + ' tulemust'
+        return render(request, 'booksearch/Search.html',
+                      {'nimistik': tulem, 'loginform': loginform,
+                       "signupform": signupform, 'tulemuste_sone': sone})
 
 
 def signout(request):
@@ -63,19 +97,8 @@ def index(request):
             return HttpResponseRedirect('')
 
         if search.is_valid():
-            sisend = search.cleaned_data['otsing']
-            tulem = raamatud.objects.filter(pealkiri__icontains=sisend)
-            cursor = connection.cursor()
-            cursor.execute("SELECT count(*) FROM booksearch_raamatud WHERE pealkiri ILIKE '%" + sisend + "%'")
-            arv = cursor.fetchone()[0]
-
-            if arv == 1:
-                sone = 'Leiti 1 tulemus'
-            else:
-                sone = 'Leiti ' + str(arv) + ' tulemust'
-            return render(request, 'booksearch/Search.html',
-                          {'nimistik': tulem, 'loginform': loginform,
-                           "signupform": signupform, 'tulemuste_sone': sone})
+            request.session['sisend'] = search.cleaned_data['otsing']
+            return redirect('search')
 
     else:
         loginform = Login
