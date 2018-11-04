c49e779aef9fbe0793311ae7ad8b37a1aa6615d7
Jaakob <37977470+JaakobJ@users.noreply.github.com>
2018-11-04 16:10:59 +0200

    Lisasin UTC märgi kellaajale.

--- static/booksearch/ipinfo.js
+++ static/booksearch/ipinfo.js
@@ -1,7 +1,7 @@
 // IP ja IP järgi riik Ajaxi abil
 var uus = document.getElementById('uus');
 $.get("https://ipinfo.io", function(response) {
-    uus.innerHTML += "<p>IP: " +  response.ip + "</p><p>Sinu Riik: " + response.country + "</p>";
+    uus.innerHTML += "<p>IP: " +  response.ip + "</p><p>Riik: " + response.country + "</p>";
 }, "jsonp").fail(function () {
-    uus.innerHTML += "<p>IP: viga, ei õnnestunud leida!</p><p>Sinu Riik: viga, ei õnnestunud leida!</p>";
+    uus.innerHTML += "<p>IP: viga, ei õnnestunud leida!</p><p>Riik: viga, ei õnnestunud leida!</p>";
 });
