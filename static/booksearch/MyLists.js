9c1c7a3b055d328ed9538531f11cf147964bc816
Jaakob <37977470+JaakobJ@users.noreply.github.com>
2018-11-18 07:05:09 +0200

    Eraldasin raamatu tekstsi ja pildi/nupud Search ja MyLists lehel. Hetkel näitab alati default pilti.

--- static/booksearch/MyLists.js
+++ static/booksearch/MyLists.js
@@ -1,12 +1,14 @@
 var owntable = document.getElementById('owntable');
 var wanttable = document.getElementById('wanttable');
-var parem = document.getElementById('parem');
+var vasak = document.getElementById('vasak');
+var pilt = document.getElementById('pilt');
 var valik;
 
 for (var i = 0; i < owntable.rows.length; i++) {
     owntable.rows[i].onclick = function() {
         valik = this.cells[0].innerHTML;
-        parem.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        vasak.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        pilt.innerHTML = '<img src="https://elisaveeb-file.elisa.ee/files/book_images/9789949781072_hirmus-henry-esimene-raamat-sari-hirmus-henri_430.jpg" alt="Hirmus Henry">';
         document.getElementById('rmWantedForm').style.display='none';
     }
 }
@@ -14,7 +16,8 @@ for (var i = 0; i < owntable.rows.length; i++) {
 for (var i = 0; i < wanttable.rows.length; i++) {
     wanttable.rows[i].onclick = function() {
         valik = this.cells[0].innerHTML;
-        parem.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        vasak.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        pilt.innerHTML = '<img src="https://elisaveeb-file.elisa.ee/files/book_images/9789949781072_hirmus-henry-esimene-raamat-sari-hirmus-henri_430.jpg" alt="Hirmus Henry">';
         document.getElementById('rmWantedForm').style.display='block';
     }
 }
--- static/booksearch/Search.js
+++ static/booksearch/Search.js
@@ -1,11 +1,13 @@
 var table = document.getElementById('table');
-var parem = document.getElementById('parem');
+var vasak = document.getElementById('vasak');
+var pilt = document.getElementById('pilt');
 var valik;
 
 for (var i = 0; i < table.rows.length; i++) {
     table.rows[i].onclick = function() {
         valik = this.cells[0].innerHTML;
-        parem.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        vasak.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        pilt.innerHTML = '<img src="https://elisaveeb-file.elisa.ee/files/book_images/9789949781072_hirmus-henry-esimene-raamat-sari-hirmus-henri_430.jpg" alt="Hirmus Henry">';
         document.getElementById('ownedForm').style.display='block';
         document.getElementById('wantedForm').style.display='block';
     }
