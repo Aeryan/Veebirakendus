40e6b49d50ed23967f421d0a3474acab72b4dbbe
Jaakob <37977470+JaakobJ@users.noreply.github.com>
2018-11-18 07:39:45 +0200

    Search & MyLists: Tegin pealkirja ja süžee kuvamise eraldi.

--- static/booksearch/MyLists.js
+++ static/booksearch/MyLists.js
@@ -1,13 +1,15 @@
 var owntable = document.getElementById('owntable');
 var wanttable = document.getElementById('wanttable');
-var vasak = document.getElementById('vasak');
+var pealkiri = document.getElementById('pealkiri');
+var tekst = document.getElementById('tekst');
 var pilt = document.getElementById('pilt');
 var valik;
 
 for (var i = 0; i < owntable.rows.length; i++) {
     owntable.rows[i].onclick = function() {
         valik = this.cells[0].innerHTML;
-        vasak.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        pealkiri.innerHTML = "<h2>" + valik + "</h2>";
+        tekst.innerHTML = "<p>Henry on üks hirmus poiss, arvavad täiskasvanud. Aga Henry ise ei tee sellest välja. Tema korraldatud ettevõtmistest ja tempudest see ja järgnevad Hirmsa Henry sarja raamatud räägivadki. Isegi kui Henry püüab olla oma venna Perfektse Peteri moodi või käia balletitunnis, on tulemus ikka võrdlemisi hirmus.</p>";
         pilt.innerHTML = '<img src="https://elisaveeb-file.elisa.ee/files/book_images/9789949781072_hirmus-henry-esimene-raamat-sari-hirmus-henri_430.jpg" alt="Hirmus Henry">';
         document.getElementById('rmWantedForm').style.display='none';
     }
@@ -16,7 +18,8 @@ for (var i = 0; i < owntable.rows.length; i++) {
 for (var i = 0; i < wanttable.rows.length; i++) {
     wanttable.rows[i].onclick = function() {
         valik = this.cells[0].innerHTML;
-        vasak.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        pealkiri.innerHTML = "<h2>" + valik + "</h2>";
+        tekst.innerHTML = "<p>Henry on üks hirmus poiss, arvavad täiskasvanud. Aga Henry ise ei tee sellest välja. Tema korraldatud ettevõtmistest ja tempudest see ja järgnevad Hirmsa Henry sarja raamatud räägivadki. Isegi kui Henry püüab olla oma venna Perfektse Peteri moodi või käia balletitunnis, on tulemus ikka võrdlemisi hirmus.</p>";
         pilt.innerHTML = '<img src="https://elisaveeb-file.elisa.ee/files/book_images/9789949781072_hirmus-henry-esimene-raamat-sari-hirmus-henri_430.jpg" alt="Hirmus Henry">';
         document.getElementById('rmWantedForm').style.display='block';
     }
--- static/booksearch/Search.js
+++ static/booksearch/Search.js
@@ -1,12 +1,14 @@
 var table = document.getElementById('table');
-var vasak = document.getElementById('vasak');
+var pealkiri = document.getElementById('pealkiri');
+var tekst = document.getElementById('tekst');
 var pilt = document.getElementById('pilt');
 var valik;
 
 for (var i = 0; i < table.rows.length; i++) {
     table.rows[i].onclick = function() {
         valik = this.cells[0].innerHTML;
-        vasak.innerHTML = "<p>Selle raamatu pealkiri on \"" + valik + "\".</p>";
+        pealkiri.innerHTML = "<h2>" + valik + "</h2>";
+        tekst.innerHTML = "<p>Henry on üks hirmus poiss, arvavad täiskasvanud. Aga Henry ise ei tee sellest välja. Tema korraldatud ettevõtmistest ja tempudest see ja järgnevad Hirmsa Henry sarja raamatud räägivadki. Isegi kui Henry püüab olla oma venna Perfektse Peteri moodi või käia balletitunnis, on tulemus ikka võrdlemisi hirmus.</p>";
         pilt.innerHTML = '<img src="https://elisaveeb-file.elisa.ee/files/book_images/9789949781072_hirmus-henry-esimene-raamat-sari-hirmus-henri_430.jpg" alt="Hirmus Henry">';
         document.getElementById('ownedForm').style.display='block';
         document.getElementById('wantedForm').style.display='block';
